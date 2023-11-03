35c5542143a1ba47741e21a7b82346ec21042f56
Isabella Basso <isabellabdoamaral@usp.br>
2023-11-02 22:02:57 -0300

    README: refer to WhisperX docs

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-Cog implementation of whisperX, a library that adds batch processing on top of whisper (and also faster-whisper), leading to very fast audio transcription. 
+# Cog-sussurroX
+
+[![Replicate](https://replicate.com/isinyaaa/whisperx/badge)](https://replicate.com/isinyaaa/whisperx)
+
+Cog predictor interface implementation of [WhisperX](https://github.com/m-bain/whisperX).
+
+## Running locally
+
+1. Install the [cog CLI](https://github.com/replicate/cog).
+2. Clone this repository
+3. Run
+
+    ```sh
+    cog predict -i audio=@/path/to/audio/file [-i other_flag=val ...]
+    ```
+
+You can check out the available flags through [Replicate](https://replicate.com/isinyaaa/whisperx).
--- predict.py
+++ predict.py
@@ -57,8 +57,6 @@ class Predictor(BasePredictor):
                 language=language,
                 print_progress=debug,
             )
-            # result is dict w/keys ['segments', 'language']
-            # segments is a list of dicts,each dict has {'text': <text>, 'start': <start_time_msec>, 'end': <end_time_msec> }
             transcription = "".join([segment["text"] for segment in result["segments"]])
             if align_output:
                 alignment_model, metadata = whisperx.load_align_model(
@@ -73,10 +71,6 @@ class Predictor(BasePredictor):
                     return_char_alignments=False,
                 )
                 result.update(aligned_result)
-                # aligned_result['word_segments'] = list[dict], each dict contains {'word': <word>, 'start': <start_time_msec>, 'end': <end_time_msec>, 'score': probability}
-                #   it is also sorted
-                # aligned_result['segments'] - same as result segments, but w/a ['words'] segment which contains timing information above.
-                # return_char_alignments adds in character level alignments. it is: too many.
             if debug:
                 print(
                     f"max gpu memory allocated over runtime: {torch.cuda.max_memory_reserved() / (1024 ** 3):.2f} GB"
