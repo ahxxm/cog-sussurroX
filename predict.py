b992342aed52ac50b5ef1c029a8d217bded94ff4
Isabella Basso <isabellabdoamaral@usp.br>
2023-11-02 22:02:57 -0300

    print progress on debug

--- predict.py
+++ predict.py
@@ -43,9 +43,7 @@ class Predictor(BasePredictor):
             description="Set if you only want to return text; otherwise, segment metadata will be returned as well.",
             default=False,
         ),
-        debug: bool = Input(
-            description="Print out memory usage information.", default=False
-        ),
+        debug: bool = Input(description="Print out debug information.", default=False),
     ) -> str:
         """Run a single prediction on the model"""
         with torch.inference_mode():
@@ -54,6 +52,7 @@ class Predictor(BasePredictor):
                 batch_size=batch_size,
                 chunk_size=chunk_size,
                 language=language,
+                print_progress=debug,
             )
             # result is dict w/keys ['segments', 'language']
             # segments is a list of dicts,each dict has {'text': <text>, 'start': <start_time_msec>, 'end': <end_time_msec> }
